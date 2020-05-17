09b4caa51cb25cf3ce2107c4d5ba5550d471b15a
alexdenvise <zetterstrom.alexander@gmail.com>
2020-05-17 19:15:51 +0200

    GUI changes

--- TestverktygProject/TestverktygProject/View/TeacherProfile.xaml.cs
+++ TestverktygProject/TestverktygProject/View/TeacherProfile.xaml.cs
@@ -26,5 +26,10 @@ namespace TestverktygProject.View
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
