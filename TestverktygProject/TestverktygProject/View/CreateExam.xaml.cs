ccc8d69e8cd2e0872c069fd963b0720feb862807
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2020-05-19 16:42:35 +0200

    Tested and tried multiple ways of generating questions. Finally gave up and trying hardcoding

--- TestverktygProject/TestverktygProject/View/CreateExam.xaml.cs
+++ TestverktygProject/TestverktygProject/View/CreateExam.xaml.cs
@@ -37,16 +37,13 @@ namespace TestverktygProject.View
             this.InitializeComponent();
             CreateExamViewModel = new CreateExamViewModel();
             this.DataContext = CreateExamViewModel.Questions;
+            
         }
 
-        private void BeforeCreationOfExamInfoButton_OnClick(object sender, RoutedEventArgs e)
+        /*private void BeforeCreationOfExamInfoButton_OnClick(object sender, RoutedEventArgs e)
         {
             int NumberOfQuestionsToGenerate = Int32.Parse(NumberOfQuestionsToGenerateField.Text);
 
-            for (int i = 0; i < NumberOfQuestionsToGenerate; i++)
-            {
-                QuestionUC = new CreateQuestionUC();
-            }
 
             /*for (int i = 0; i < NumberOfQuestionsToGenerate; i++)
             {
@@ -57,9 +54,9 @@ namespace TestverktygProject.View
                 CreateQuestionsStackPanel.Children.Add(new TextBox() { PlaceholderText = "alternative 4", Name = "Alt3" });
                 CreateQuestionsStackPanel.Children.Add(new TextBlock() { Text = "The right answer is: ", Name = "CorrectAnswer" });
                 CreateQuestionsStackPanel.Children.Add(new TextBox() { PlaceholderText = "Answer with one digit", Name = "CorrectAnswerField" });
-            }*/
+            }
 
-        }
+        }*/
 
         private void CreateExamButton_OnClick(object sender, RoutedEventArgs e)
         {
@@ -75,13 +72,24 @@ namespace TestverktygProject.View
         private void SubmitQuestionButton_OnClick(object sender, RoutedEventArgs e)
         {
             //GetElementFromCreatedQuestions();
-            
+            alternatives.Add(Alt1.Text);
+            alternatives.Add(Alt2.Text);
+            alternatives.Add(Alt3.Text);
+            alternatives.Add(Alt4.Text);
+
+            RightAnswer.Add(Int32.Parse(CorrectAnswerField.Text));
+
+            opt1.Content = Alt1.Text;
+            opt2.Content = Alt2.Text;
+            opt3.Content = Alt3.Text;
+            opt4.Content = Alt4.Text;
+
             Question question = new Question
             {
                 Alternatives = alternatives,
                 CorrectAnswer = RightAnswer,
                 NumberOfPoints = 1,
-                QuestionTitle = "Some text"
+                QuestionTitle = TitleOfQuestion.Text
                 //TODO edit number of points so that the teacher decides the point per question
             };
 
@@ -101,32 +109,40 @@ namespace TestverktygProject.View
 
         /*public void GetElementFromCreatedQuestions()
         {
+            object WA1 = CreateQuestionsStackPanel.FindName("Alt1");
+            object WA2 = CreateQuestionsStackPanel.FindName("Alt2");
+            object WA3 = CreateQuestionsStackPanel.FindName("Alt3");
+            object WA4 = CreateQuestionsStackPanel.FindName("Alt4");
+            object RA = CreateQuestionsStackPanel.FindName("CorrectAnswerField");
+
+            object WantedQuestionTitle = CreateQuestionsStackPanel.FindName("TitleOfQuestion");
             foreach (UIElement child in CreateQuestionsStackPanel.Children)
             {
                 if (child is StackPanel)
                 {
                     foreach (UIElement ctrlChild in (child as StackPanel).Children)
                     {
-                        if (ctrlChild is TextBox)
+                        if (ctrlChild is TextBox && WA1 is TextBox && WA2 is TextBox && WA3 is TextBox && WA4 is TextBox)
                         {
                             alternatives.Add(ctrlChild.ToString());
+                        }
+
+                        if (ctrlChild is TextBox && RA is TextBox)
+                        {
+                            RightAnswer.Add(Int32.Parse(ctrlChild.ToString()));
                         }
                     }
                 }
             }
+        }*/
 
-            foreach (UIElement tb in CreateQuestionsStackPanel.Children)
-            {
-                if (tb is TextBox)
-                {
-                    RightAnswer.Add(Int32.Parse(tb.ToString()));
-                }
-            }
-        }
-
-        public UIElement GetQuestionTitle()
+        /*public object GetQuestionTitle()
         {
-            
+            object TitleOfQuestion = CreateQuestionsStackPanel.FindName("TitleOfQuestion");
+
+
+            return TitleOfQuestion;
+
         }*/
     }
 }
