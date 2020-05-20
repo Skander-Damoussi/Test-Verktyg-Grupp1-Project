128f8f961275d9f2ea31113241de2cffa7e0dcdd
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2020-05-20 17:17:31 +0200

    removed hardcoded questions, and added some properties for binding purposes

--- TestverktygProject/TestverktygProject/ViewModel/CreateExamViewModel.cs
+++ TestverktygProject/TestverktygProject/ViewModel/CreateExamViewModel.cs
@@ -4,65 +4,34 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using TestverktygProject.Model;
+using TestverktygProject.View;
 
 namespace TestverktygProject.ViewModel
 {
     public class CreateExamViewModel
     {
         public ObservableCollection<Exam> ExamList { get; set; }
-        public ObservableCollection<Question> Questions { get; set; }
+        public ObservableCollection<Question> QuestionsToBeFilled { get; set; }
+        public ObservableCollection<Question> CreatedQuestions { get; set; }
         public Question Question { get; set; }
         public Exam Exam { get; set; }
+        public string TB { get; set; }
+
+        public ICommand SearchBusCommand { get; }
+
+        public string SearchText { get; set; }
+
+
+        //Perhaps need to move below properties to viewmodel constructor.
+        public List<string> Alternatives { get; set; }
         public CreateExamViewModel()
         {
             ExamList = new ObservableCollection<Exam>();
-            Questions = new ObservableCollection<Question>();
-
-            Questions.Add(new Question()
-            {
-                Alternatives = new List<string>()
-                {
-                    "1", "2","3","4"
-                },
-                CorrectAnswer = new List<int>()
-                {
-                    1
-                },
-                NumberOfPoints = 1,
-                QuestionTitle = "Fråga 1",
-
-            });
-
-            Questions.Add(new Question()
-            {
-                Alternatives = new List<string>()
-                {
-                    "1", "2","3","4"
-                },
-                CorrectAnswer = new List<int>()
-                {
-                    1
-                },
-                NumberOfPoints = 1,
-                QuestionTitle = "Fråga 2",
-
-            });
-
-            Questions.Add(new Question()
-            {
-                Alternatives = new List<string>()
-                {
-                    "1", "2","3","4"
-                },
-                CorrectAnswer = new List<int>()
-                {
-                    1
-                },
-                NumberOfPoints = 1,
-                QuestionTitle = "Fråga 3",
-
-            });
+            QuestionsToBeFilled = new ObservableCollection<Question>();
+            CreatedQuestions = new ObservableCollection<Question>();
+            Alternatives = new List<string>();
         }
     }
 }
