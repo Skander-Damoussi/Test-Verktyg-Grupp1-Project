ddce56f4dc2229a9b48170555a6e735e49a4123e
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2020-05-22 20:26:35 +0200

    cleanup and changed a few methods

--- TestverktygProject/TestverktygProject/View/CreateExam.xaml.cs
+++ TestverktygProject/TestverktygProject/View/CreateExam.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Devices.Sensors;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using System.Collections.Generic;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using TestverktygProject.Model;
 using TestverktygProject.ViewModel;
 
@@ -25,16 +13,9 @@ namespace TestverktygProject.View
     /// </summary>
     public sealed partial class CreateExam : Page
     {
-        private List<Question> listOfQuestions = new List<Question>();
-        private List<Exam> listOfExams = new List<Exam>();
-
-        List<string> alternatives = new List<string>();
         List<int> RightAnswer = new List<int>();
 
         public CreateExamViewModel CreateExamViewModel { get; set; }
-        public CreateQuestionUC QuestionUC { get; set; }
-        public Exam ExamProperty { get; set; }
-        public Question QProp { get; set; }
 
 
         public CreateExam()
@@ -42,7 +23,6 @@ namespace TestverktygProject.View
             this.InitializeComponent();
             CreateExamViewModel = new CreateExamViewModel();
             this.DataContext = CreateExamViewModel;
-            QuestionsToBeFilledListView.ItemsSource = CreateExamViewModel.QuestionsToBeFilled;
             NotYetCreatedExamListView.ItemsSource = CreateExamViewModel.CreatedQuestions;
             SeeCreatedExamListView.ItemsSource = CreateExamViewModel.ExamList;
 
@@ -51,20 +31,16 @@ namespace TestverktygProject.View
 
         private void BeforeCreationOfExamInfoButton_OnClick(object sender, RoutedEventArgs e)
         {
-            int NumberOfQuestionsToGenerate = Int32.Parse(NumberOfQuestionsToGenerateField.Text);
+            var numberOfQuestionsToGenerate = int.Parse(NumberOfQuestionsToGenerateField.Text);
+            var questions = CreateExamViewModel.QuestionsToBeFilled;
+            var previewExam = CreateExamViewModel.CreatedQuestions;
 
+            questions.Clear();
+            previewExam.Clear();
 
-            for (int i = 0; i < NumberOfQuestionsToGenerate; i++)
+            for (var i = 0; i < numberOfQuestionsToGenerate; i++)
             {
                 CreateExamViewModel.QuestionsToBeFilled.Add(new Question());
-                
-                /*CreateQuestionsStackPanel.Children.Add(new TextBox() { PlaceholderText = "Title of question", Name = "TitleOfQuestion" });
-                CreateQuestionsStackPanel.Children.Add(new TextBox() { PlaceholderText = "alternative 1", Name = "Alt1" });
-                CreateQuestionsStackPanel.Children.Add(new TextBox() { PlaceholderText = "alternative 2", Name = "Alt2" });
-                CreateQuestionsStackPanel.Children.Add(new TextBox() { PlaceholderText = "alternative 3", Name = "Alt3" });
-                CreateQuestionsStackPanel.Children.Add(new TextBox() { PlaceholderText = "alternative 4", Name = "Alt3" });
-                CreateQuestionsStackPanel.Children.Add(new TextBlock() { Text = "The right answer is: ", Name = "CorrectAnswer" });
-                CreateQuestionsStackPanel.Children.Add(new TextBox() { PlaceholderText = "Answer with one digit", Name = "CorrectAnswerField" });*/
                 
             }
 
@@ -72,49 +48,29 @@ namespace TestverktygProject.View
 
         private void CreateExamButton_OnClick(object sender, RoutedEventArgs e)
         {
-            Exam exam = new Exam
+            var exam = new Exam
             {
-                ExamID = ExamProperty.ExamID,
+                /*ExamID = ExamProperty.ExamID,
                 ExamDate = ExamProperty.ExamDate,
                 ExamName = ExamProperty.ExamName,
                 Questions = ExamProperty.Questions,
                 Subject = ExamProperty.Subject,
-                Results = ExamProperty.Results
+                Results = ExamProperty.Results*/
             };
         }
 
         private void SubmitQuestionButton_OnClick(object sender, RoutedEventArgs e)
         {
-            
-                alternatives.Add("One");
-                alternatives.Add("two");
-                alternatives.Add("three");
-                alternatives.Add("four");
+            var questions = CreateExamViewModel.QuestionsToBeFilled;
+            var examPreview = CreateExamViewModel.CreatedQuestions;
 
-           RightAnswer.Add(Int32.Parse("1"));
+            examPreview.Clear();
 
-            Question question = new Question
+            foreach (Question q in questions)
             {
-                Alternatives = alternatives,
-                CorrectAnswer = RightAnswer,
-                NumberOfPoints = 1,
-                QuestionTitle = CreateExamViewModel.QuestionTitle
-                //TODO edit number of points so that the teacher decides the point per question
-            };
+                CreateExamViewModel.CreatedQuestions.Add(q);
+            }
 
-            listOfQuestions.Add(question);
-            CreateExamViewModel.CreatedQuestions.Add(question);
-            
-        }
-
-        public List<Question> GetQuestions()
-        {
-            return listOfQuestions;
-        }
-
-        public List<Exam> GetExams()
-        {
-            return listOfExams;
         }
     }
 }
