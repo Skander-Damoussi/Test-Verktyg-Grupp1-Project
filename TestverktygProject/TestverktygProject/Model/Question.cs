b7946ba7fc8b0c6acc533df385298318390c502d
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2020-05-22 10:34:20 +0200

    cleanup in question class

--- TestverktygProject/TestverktygProject/Model/Question.cs
+++ TestverktygProject/TestverktygProject/Model/Question.cs
@@ -8,15 +8,10 @@ namespace TestverktygProject.Model
 {
     public class Question
     {
-        public List<int> CorrectAnswer { get; set; }
-        public List<string> Alternatives { get; set; }
+        public List<int> CorrectAnswer { get; set; } = new List<int>();
+        public List<string> Alternatives { get; set; } = new List<string>();
         public int NumberOfPoints { get; set; }
         public string QuestionTitle { get; set; }
-
-        public string Alt1 { get; set; }
-        public string Alt2 { get; set; }
-        public string Alt3 { get; set; }
-        public string Alt4 { get; set; }
 
         public Question(List<int> correctanswer, List<string> alternative, int numberofpoints, string questiontitle)
         {
