5aff9cea0c810b341a4e3cb2d7a5804cc4cdfb71
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2020-05-23 21:42:38 +0200

    Added local db and inserted connectionstring for remote db

---
+++ TestVerktygAPI/TestVerktygAPI/Migrations/20200523191842_AM1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestVerktygAPI.Migrations
+{
+    public partial class AM1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
