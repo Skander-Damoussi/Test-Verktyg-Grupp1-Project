89044f561eba486765eb9d50250fbe7e44563f89
Saimir avdija <katundari_@hotmail.com>
2020-05-25 13:01:42 +0200

    Signoutbutton changed, made it possible to select/start an exam through student page

--- TestverktygProject/TestverktygProject/View/StudentProfile.xaml.cs
+++ TestverktygProject/TestverktygProject/View/StudentProfile.xaml.cs
@@ -8,6 +8,7 @@ using TestverktygProject.Model;
 using TestverktygProject.ViewModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -39,6 +40,24 @@ namespace TestverktygProject.View
             //tempstudent = Vm.student1;
             FirstNameText.Text = tempstudent.FirstName;
             LastNameText.Text = tempstudent.LastName;
+        }
+
+        private void startExamButton_Click(object sender, RoutedEventArgs e)
+        {
+            Exam selectedExam = (Exam)StudentsExam.SelectedItem;
+            this.Frame.Navigate(typeof(TakeExam),selectedExam);
+        }
+
+        private async void signOutButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageDialog confirmDialog = new MessageDialog("Do you want to sign out?", "Sign out confirmation");
+            confirmDialog.Commands.Add(new UICommand("Yes"));
+            confirmDialog.Commands.Add(new UICommand("No"));
+            var confirmResult = await confirmDialog.ShowAsync();
+            // "No" button pressed: Keep the app open.
+            if (confirmResult != null && confirmResult.Label == "No") { return; }
+            // "Back" or "Yes" button pressed: Close the app.
+            if (confirmResult == null || confirmResult.Label == "Yes") { Frame.Navigate(typeof(LogIn)); }
         }
     }
 }
