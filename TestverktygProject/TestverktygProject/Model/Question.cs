d88c6e03709314d563246cac240dc3d480eecd99
Anna-Maria Eriksson <greatdanemaniac@gmail.com>
2020-05-28 16:11:36 +0200

    Removed bool

--- TestverktygProject/TestverktygProject/Model/Question.cs
+++ TestverktygProject/TestverktygProject/Model/Question.cs
@@ -11,14 +11,12 @@ namespace TestverktygProject.Model
     {
         public int CorrectAnswer { get; set; }
         public List<string> Alternatives { get; set; } = new List<string>{"", "", "", ""};
-
         public string Alt1 { get; set; }
         public string Alt2 { get; set; }
         public string Alt3 { get; set; }
         public string Alt4 { get; set; }
         public int NumberOfPoints { get; set; }
         public string QuestionTitle { get; set; }
-        public bool IsCorrectAnswer { get; set; }
 
         public Question(int correctanswer, List<string> alternative, int numberofpoints, string questiontitle)
         {
